9188b314425825af9a11a5b2812ab4c5df9154e2
Roman Terry <reterry02@gmail.com>
2024-12-11 16:22:41 -0600

    Built maven project with Spring Initializr and built Character model

---
+++ project1/src/main/java/com/revature/project1/model/Character.java
@@ -0,0 +1,105 @@
+package com.revature.project1.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "characters")
+public class Character {
+    /*
+            - id (int) (primary key)
+            - name (varchar)
+            - age (int)
+            - description (varchar)
+            - setting (varchar) (maybe hold off until later)
+            - creator (varchar) (foreign key)
+            - public (boolean)
+            - flagFor18Plus (boolean)
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int characterId;
+
+    private String characterName;
+
+    private int characterAge;
+
+    private String description;
+
+    //Holds the userId of the user who created the character
+    private int creator;
+
+    //If true, the character will be visible to all users. If false, the character will only be visible to its creator and all admins
+    private boolean isPublic;
+
+    //If true, the character will not appear to users who do not wish to see mature content
+    private boolean matureContent;
+
+    //Constructors
+    public Character(){};
+
+    public Character(int characterId, String characterName, int characterAge, String description, int creator, boolean isPublic, boolean matureContent) {
+        this.characterId = characterId;
+        this.characterName = characterName;
+        this.characterAge = characterAge;
+        this.description = description;
+        this.creator = creator;
+        this.isPublic = isPublic;
+        this.matureContent = matureContent;
+    }
+
+    //Getters and Setters
+    public int getCharacterId() {
+        return characterId;
+    }
+
+    public void setCharacterId(int characterId) {
+        this.characterId = characterId;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public int getCharacterAge() {
+        return characterAge;
+    }
+
+    public void setCharacterAge(int characterAge) {
+        this.characterAge = characterAge;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreator() {
+        return creator;
+    }
+
+    public void setCreator(int creator) {
+        this.creator = creator;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public boolean isMatureContent() {
+        return matureContent;
+    }
+
+    public void setMatureContent(boolean matureContent) {
+        this.matureContent = matureContent;
+    }
+}
